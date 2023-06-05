144d2ba4d5471ddaf1384cab0dcda3e07470e68b
Michael Kharchenko <michael@realplusonline.com>
2023-06-05 22:13:52 +0300

    Add nvim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,18 @@
+-- Packer Setup
+require("config/packer")
+
+-- Core Settings
+require("config/core/options")
+require("config/core/colorscheme")
+require("config/core/keymaps")
+
+-- Plugins
+require("config/plugins/treesitter")
+require("config/plugins/autocompletion")
+require("config/plugins/autopairs")
+require("config/plugins/comment")
+
+-- LSP
+require("config/plugins/lsp/mason")
+require("config/plugins/lsp/lspconfig")
+require("config/plugins/lsp/null-ls")
---
+++ nvim/lua/config/core/options.lua
@@ -0,0 +1,20 @@
+vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
