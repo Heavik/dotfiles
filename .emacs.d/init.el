3172023cc21b073cfe4f72a954ff711d1c3f27e1
Mike Kharchenko <kahrchenkoms@gmail.com>
2025-04-16 18:02:15 +0000

    Add custom duplicate-line function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -103,6 +103,20 @@
 	      ("M-." . xref-find-definitions)
 	      ("C-c C-f" . eglot-format-buffer)))
 
+(defun mycustom/duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (point-at-bol)))
+        (line (let ((s (thing-at-point 'line t)))
+                (if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (forward-char column)))
+
+(global-set-key (kbd "C-,") 'mycustom/duplicate-line)
+
 ;; BEGIN Tree-sitter configuration
 (setq treesit-language-source-alist
       '((c "https://github.com/tree-sitter/tree-sitter-c")
