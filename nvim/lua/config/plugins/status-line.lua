baceac973e620c118344b5cc7a3dbedf3c76eed0
Mike Kharchenko <kahrchenkoms@gmail.com>
2025-07-19 17:02:00 +0000

    remove old nvim config

--- nvim/lua/config/plugins/status-line.lua
+++
@@ -1,9 +0,0 @@
--- import lualine plugin safely
-local lualine_setup, lualine = pcall(require, "lualine")
-if not lualine_setup then
-	return
-end
-
-lualine.setup({
-	theme = "gruvbox-material",
-})
