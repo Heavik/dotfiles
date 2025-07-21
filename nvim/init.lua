fd26ef61eb9bae8351ce05ce0830b112b00346ae
Mike Kharchenko <kahrchenkoms@gmail.com>
2025-07-21 18:32:26 +0000

    Add basic plugins setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,10 +36,54 @@ vim.opt.path:append("**")
 vim.opt.modifiable = true
 vim.opt.encoding = "UTF-8"
 
--------- Key Bindings -----
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-------- Plugins -------- 
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." }
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_background = "soft"
+            vim.cmd.colorscheme('gruvbox-material')
+        end
+    },
+    
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" }
+})
+
+require("cmp").setup({
+    sources = {
+        { name = "buffer" },
+        { name = "path" }
+    }
+})
+
+-------- Key Bindings -----
 vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
 
 vim.keymap.set("i", "<C-u>", "<C-g>u<C-u>")
